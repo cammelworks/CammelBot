2cc7653b64ab3d85d3ba83bd1bca01313e6611d4
Masaya Kugita <40158101+kugimasa@users.noreply.github.com>
2019-08-04 12:17:32 +0900

    hide token

--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-522308573394-703879037635-WqutrbDClZaWd4pAFwuMO9Rw"
+API_TOKEN = "XXXXX-XXXXXX"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "メェェェ"
