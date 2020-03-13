71f5d1a566ea1b4d23feb5119188306b578a159f
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-03-13 19:05:07 +0900

    parrot return

---
+++ slackbot/plugins/my_mention.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+from slackbot.bot import default_reply
+import re
+from datetime import datetime
+
+@respond_to('time')
+def now(message):
+    strftime = datetime.now().strftime("%Y/%m/%d %H時%M分%S秒です")
+    message.reply(strftime)
+
+@respond_to('今何時')
+def now_time(message):
+    strftime = datetime.now().strftime("%Y/%m/%d %H時%M分%S秒です")
+    message.reply(strftime)
+
+@respond_to('あ')
+def test(message):
+    message.reply("test")
+
+@default_reply()
+def default(message):
+    mes = message.body['text']
+    message.reply(mes)
--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -4,7 +4,7 @@
 API_TOKEN = "XXXXX-XXXXXX"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
-DEFAULT_REPLY = "メェェェ"
+# DEFAULT_REPLY = "メェェェ"
 
 # プラグインスクリプトを置いてあるサブディレクトリ名のリスト
 PLUGINS = ['plugins']
