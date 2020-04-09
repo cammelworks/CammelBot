c1b2d0e95954832005b9f0e106433fede1a32b8e
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-04-09 14:11:55 +0900

    modify setting

--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 # botアカウントのトークンを指定
-# API_TOKEN = "XXXXX-XXXXXX"
+API_TOKEN = "xoxb-522308573394-703879037635-vryhemzzbKa22CRAYfTGoQFs"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 # DEFAULT_REPLY = "メェェェ"
