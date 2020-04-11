947bdafdf3a82ae5eab9a69b1f6de4075cc3db3c
kugi <kugimania@gmail.com>
2020-04-11 22:32:28 +0900

    Set API TOKEN from environ

--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -1,7 +1,7 @@
 # coding: utf-8
-
+import os
 # botアカウントのトークンを指定
-# API_TOKEN = "XXXXX-XXXXXX"
+API_TOKEN = os.environ['API_TOKEN']
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 # DEFAULT_REPLY = "メェェェ"
